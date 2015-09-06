6f75d04031f6ad9064ea656ed2b1b167da7f3564
Nick Chadeayne <nrchade@yahoo.com>
2015-09-05 21:27:06 -0500

    Intial commit of yahoo FF

---
+++ GetYahooFFIntel.R
@@ -0,0 +1,91 @@
+library(httr)
+library(XML)
+library(httpuv)
+library(RJSONIO)
+library(ggplot2)
+library(rvest)
+
+#store they key in the text files, or paste them in here.
+creds <- read.csv("C:/Users/Brooke/Downloads/YahooAPI.csv", stringsAsFactors=F)
+consumer.key <- creds[1,2]
+consumer.secret <- creds[2,2]
+
+
+#standard authorization steps from httr
+yahoo<-oauth_endpoints("yahoo")
+myapp <- oauth_app("yahoo", key = consumer.key, secret = consumer.secret)
+token <- oauth1.0_token(oauth_endpoints("yahoo"), myapp, as_header = FALSE, cache = FALSE)
+
+# need to get game id for my league...
+ff.url <- "http://fantasysports.yahooapis.com/fantasy/v2/game/nfl?format=json"
+game.key.json <- GET(ff.url, config(token = token))
+game.key.list <- fromJSON(as.character(game.key.json), asText=T)
+game.key <- game.key.list$fantasy_content$game[[1]]["game_key"]
+
+# my personal leagueid, you will have to use your own, mine is private
+game.id <- "348"
+game.key <- game.id
+game.url <- "http://fantasysports.yahooapis.com/fantasy/v2/game/"
+
+
+
+# my personal leagueid, you will have to use your own, mine is private
+league.id <- "982118"
+league.key <- paste0(game.key, ".l.", league.id)
+league.url <- "http://fantasysports.yahooapis.com/fantasy/v2/league/"
+
+my.team.id <- "2"
+my.team.key <- paste0(league.key, ".t.", my.team.id)
+team.url <- "http://fantasysports.yahooapis.com/fantasy/v2/team/"
+
+#getting transactions although the process works well for digging into most collections
+responseAllTeams<- html(paste0(league.url, league.key, "/teams"), config(token = token))
+#responseAllTeams %>% xml_structure()
+Name <- responseAllTeams %>% html_nodes('name')
+
+responseAllPlayersOnRoster<- html(paste0(team.url, my.team.key, "/roster/players"), config(token = token))
+#responseAllPlayersOnRoster %>% xml_structure()
+#Players <- responseAllPlayers %>% html_nodes('player')
+Names.df <- responseAllPlayersOnRoster %>% html_nodes('player') %>% html_nodes('full') %>% html_text()
+Position.df <- responseAllPlayersOnRoster %>% html_nodes('player') %>% html_nodes('selected_position')%>% html_nodes('position') %>% html_text()
+Roster.df <- data.frame(Names.df,Position.df)
+
+url <- paste0(league.url, league.key,"/players?&status=A&stat1=S_PW_1&start=37")
+responseAllPlayersFree <- html("http://fantasysports.yahooapis.com/fantasy/v2/league/348.l.982118/players?&status=A&start=0&stat1=S_PW_1",config(token=token))
+responseAllPlayersFree<- html(url, config(token = token))
+#responseAllPlayersFree %>% xml_structure()
+#NamesFree.df <- responseAllPlayersFree %>% html_nodes('players') %>% html_nodes('full') %>% html_text()
+#PositionFree.df <- responseAllPlayersFree %>% html_nodes('players') %>%  html_nodes('eligible_positions') %>% html_text()
+#AllPlayersFree <- data.frame(NamesFree.df,PositionFree.df)
+
+baseurl <- "http://fantasysports.yahooapis.com/fantasy/v2/league/348.l.982118/players?&status=A"
+playerCount <- paste("&start=",seq(0,1050,by=25),sep="")
+PWeekCount<-paste("&stat1=S_PW_",1:17,sep="")
+YahooURLs <- unlist(lapply(PWeekCount,function(x)paste(baseurl,playerCount,x,sep="")))
+
+yahoo<-lapply(YahooURLs,function(x) {Sys.sleep(abs(rnorm(1)*4+7));html(x, config(token = token))})
+
+
+for (i in 1:(length(yahoo)) ) {
+  Names <- yahoo[[i]] %>% html_nodes('players') %>% html_nodes('full') %>% html_text()
+  Player_id <- yahoo[[i]] %>% html_nodes('players') %>% html_nodes('player_id') %>% html_text()
+  Position <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('position_type') %>% html_text()
+  Eligible <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('eligible_positions') %>% html_text()
+  ByeWeek <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('bye_weeks') %>% html_text()
+  IsDroppable <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('is_undroppable') %>% html_text()
+  Team <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('editorial_team_full_name') %>% html_text()
+  Notes <- yahoo[[i]] %>% html_nodes('players') %>%  html_nodes('has_player_notes') %>% html_text()
+    if (i == 1) {AllPlayersFree <- data.frame(Names,Player_id,Position,Team,Eligible,ByeWeek,IsDroppable,Notes)}
+  else {AllPlayersFree <- rbind(AllPlayersFree,data.frame(Names,Player_id,Position,Team,Eligible,ByeWeek,IsDroppable,Notes))}
+rm(Names)
+rm(Player_id)
+rm(Position)
+rm(Eligible)
+rm(ByeWeek)
+rm(IsDroppable)
+rm(Team
+rm(Notes))
+
+
+}
+  
