3d21aa500d8e418b4f964578c164d6c2fc293ba2
Nick Chadeayne <nrchade@yahoo.com>
2015-10-10 21:57:50 -0500

    Change html function to read_html as it seem html is obsolete

--- GetYahooFFIntel.R
+++ GetYahooFFIntel.R
@@ -6,7 +6,7 @@ library(ggplot2)
 library(rvest)
 
 #store they key in the text files, or paste them in here.
-creds <- read.csv("C:/Users/Brooke/Downloads/YahooAPI.csv", stringsAsFactors=F)
+creds <- read.csv(paste0(getwd(),"/YahooAPI.csv"), stringsAsFactors=F)
 consumer.key <- creds[1,2]
 consumer.secret <- creds[2,2]
 
@@ -17,10 +17,10 @@ myapp <- oauth_app("yahoo", key = consumer.key, secret = consumer.secret)
 token <- oauth1.0_token(oauth_endpoints("yahoo"), myapp, as_header = FALSE, cache = FALSE)
 
 # need to get game id for my league...
-ff.url <- "http://fantasysports.yahooapis.com/fantasy/v2/game/nfl?format=json"
-game.key.json <- GET(ff.url, config(token = token))
-game.key.list <- fromJSON(as.character(game.key.json), asText=T)
-game.key <- game.key.list$fantasy_content$game[[1]]["game_key"]
+#ff.url <- "http://fantasysports.yahooapis.com/fantasy/v2/game/nfl?format=json"
+#game.key.json <- GET(ff.url, config(token = token))
+#game.key.list <- fromJSON(as.character(game.key.json), asText=T)
+#game.key <- game.key.list$fantasy_content$game[[1]]["game_key"]
 
 # my personal leagueid, you will have to use your own, mine is private
 game.id <- "348"
@@ -39,11 +39,11 @@ my.team.key <- paste0(league.key, ".t.", my.team.id)
 team.url <- "http://fantasysports.yahooapis.com/fantasy/v2/team/"
 
 #getting transactions although the process works well for digging into most collections
-responseAllTeams<- html(paste0(league.url, league.key, "/teams"), config(token = token))
+responseAllTeams<- read_html(paste0(league.url, league.key, "/teams"), config(token = token))
 #responseAllTeams %>% xml_structure()
 Name <- responseAllTeams %>% html_nodes('name')
 
-responseAllPlayersOnRoster<- html(paste0(team.url, my.team.key, "/roster/players"), config(token = token))
+responseAllPlayersOnRoster<- read_html(paste0(team.url, my.team.key, "/roster/players"), config(token = token))
 #responseAllPlayersOnRoster %>% xml_structure()
 #Players <- responseAllPlayers %>% html_nodes('player')
 Names.df <- responseAllPlayersOnRoster %>% html_nodes('player') %>% html_nodes('full') %>% html_text()
@@ -51,8 +51,8 @@ Position.df <- responseAllPlayersOnRoster %>% html_nodes('player') %>% html_node
 Roster.df <- data.frame(Names.df,Position.df)
 
 url <- paste0(league.url, league.key,"/players?&status=A&stat1=S_PW_1&start=37")
-responseAllPlayersFree <- html("http://fantasysports.yahooapis.com/fantasy/v2/league/348.l.982118/players?&status=A&start=0&stat1=S_PW_1",config(token=token))
-responseAllPlayersFree<- html(url, config(token = token))
+responseAllPlayersFree <- read_html("http://fantasysports.yahooapis.com/fantasy/v2/league/348.l.982118/players?&status=A&start=0&stat1=S_PW_1",config(token=token))
+responseAllPlayersFree<- read_html(url, config(token = token))
 #responseAllPlayersFree %>% xml_structure()
 #NamesFree.df <- responseAllPlayersFree %>% html_nodes('players') %>% html_nodes('full') %>% html_text()
 #PositionFree.df <- responseAllPlayersFree %>% html_nodes('players') %>%  html_nodes('eligible_positions') %>% html_text()
@@ -63,7 +63,7 @@ playerCount <- paste("&start=",seq(0,1050,by=25),sep="")
 #PWeekCount<-paste("&stat1=S_PW_",1:17,sep="")
 YahooURLs <- unlist((paste(baseurl,playerCount,sep="")))
 
-yahooHTML<-lapply(YahooURLs,function(x) {Sys.sleep(abs(rnorm(1)*4+7));html(x, config(token = token))})
+yahooHTML<-lapply(YahooURLs,function(x) {Sys.sleep(abs(rnorm(1)*4+7));read_html(x, config(token = token))})
 
 
 for (i in 1:(length(yahooHTML)) ) {
@@ -95,9 +95,9 @@ for (i in 1:(length(yahooHTML)) ) {
 #PWeekCount<-paste("&stat1=S_PW_",1:17,sep="")
 #YahooURLs <- unlist((paste(baseurl,playerCount,sep="")))
 
-#yahooHTML<-lapply(YahooURLs,function(x) {Sys.sleep(abs(rnorm(1)*4+7));html(x)})#, config(token = token))})
+#yahooHTML<-lapply(YahooURLs,function(x) {Sys.sleep(abs(rnorm(1)*4+7));read_html(x)})#, config(token = token))})
 
-#html <- html(yahooHTML[[1]])
+#html <- read_html(yahooHTML[[1]])
 #cast <- html_nodes(html, "a")
 
 
